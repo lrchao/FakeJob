d027c5cbf0aa7eb3136583c72b0e5918994a986f
liuranchao <lrc19860926@gmail.com>
2017-09-17 15:37:53 +0800

    搜索页面

--- library/src/main/java/com/lrchao/fakejob/ui/activity/search/SearchFragment.java
+++ library/src/main/java/com/lrchao/fakejob/ui/activity/search/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.lrchao.fakejob.ui.activity.search;
 
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import com.lrchao.fakejob.mvp.BasePresenter;
 import com.lrchao.fakejob.ui.activity.common.BaseFragment;
 import com.lrchao.fakejob.ui.activity.common.NetworkFragment;
 import com.lrchao.fakejob.ui.views.location.EditTextWithDel;
+import com.lrchao.fakejob.utils.NavUtils;
 import com.lrchao.utils.SoftKeyboardUtils;
 
 /**
@@ -19,7 +21,7 @@ import com.lrchao.utils.SoftKeyboardUtils;
  * @date 2017/9/4 上午11:18
  */
 
-public class SearchFragment extends NetworkFragment implements SearchContract.View {
+public class SearchFragment extends NetworkFragment implements SearchContract.View, View.OnClickListener {
 
     private EditTextWithDel mEditTextWithDel;
 
@@ -56,17 +58,52 @@ public class SearchFragment extends NetworkFragment implements SearchContract.Vi
                 if ((keyEvent != null && keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) ||
                         (actionId == KeyEvent.KEYCODE_ENDCALL)) {
                     SoftKeyboardUtils.hideKeyboard(getContext(), mEditTextWithDel);
+
+                    if (!TextUtils.isEmpty(mEditTextWithDel.getText())) {
+                        NavUtils.get().navToCategory(getContext(), mEditTextWithDel.getText().toString(), 1);
+                    }
+
                     return true;
                 }
                 return false;
             }
         });
 
+        parentView.findViewById(R.id.tv_app).setOnClickListener(this);
+        parentView.findViewById(R.id.tv_online).setOnClickListener(this);
+        parentView.findViewById(R.id.tv_offline).setOnClickListener(this);
+        parentView.findViewById(R.id.tv_kfc).setOnClickListener(this);
+        parentView.findViewById(R.id.tv_mdl).setOnClickListener(this);
     }
 
     @Override
     protected BasePresenter createPresenter() {
         mPresenter = new SearchPresenter();
         return mPresenter;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.tv_app) {
+
+            NavUtils.get().navToCategory(getContext(), "APP", 2);
+
+        } else if (view.getId() == R.id.tv_online) {
+
+            NavUtils.get().navToCategory(getContext(), "线上兼职", 3);
+
+        } else if (view.getId() == R.id.tv_offline) {
+
+            NavUtils.get().navToCategory(getContext(), "线下兼职", 1);
+
+        } else if (view.getId() == R.id.tv_kfc) {
+
+            NavUtils.get().navToCategory(getContext(), "肯德基", 2);
+
+        } else if (view.getId() == R.id.tv_mdl) {
+
+            NavUtils.get().navToCategory(getContext(), "麦当劳", 3);
+
+        }
     }
 }
